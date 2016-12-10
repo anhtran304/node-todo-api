97192e0a8372b05da0abff25926586ba506bcb1e
Kentran2809 <tranmr@gmail.com>
2016-12-10 17:56:36 +1100

    Edit Model Todo - required: true

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,7 +5,7 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -21,31 +21,31 @@ describe('POST /todos', () => {
       })
       .end((err, res) => {
         if (err) {
-          return done(err)
+          return done(err);
         }
 
         Todo.find().then((todos) => {
           expect(todos.length).toBe(1);
-          expect(res.body.text).toBe(text);
+          expect(todos[0].text).toBe(text);
           done();
         }).catch((e) => done(e));
       });
   });
 
-    it('should not create todo with invalid body data', (done) => {
-      request(app)
-        .post('/todos')
-        .send({})
-        .expect(400)
-        .end((err, res) => {
-          if (err) {
-            return done(err)
-          }
-          Todo.find().then((todos) => {
-            expect(todos.length).toBe(0);
-            done();
-          }).catch((e) => done(e));
-        });
-    });
+  it('should not create todo with invalid body data', (done) => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
 
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
 });
