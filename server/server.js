631da25472b35da23c3d8a7c92b2d2fed71ba133
Kentran2809 <tranmr@gmail.com>
2016-12-12 23:46:28 +1100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -34,14 +34,17 @@ app.get('/todos/:id', (req, res) => {
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
-  } Todo.findById(id).then((todo) => {
+  }
 
-  }, (e) => {
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
 
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
-
-}, (e) => {
-  res.status(400).send();
 });
 
 app.listen(3000, () => {
