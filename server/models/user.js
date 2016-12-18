03fa7b1ff3a031afe1ab6d018b8ab049f5642ed2
Kentran2809 <tranmr@gmail.com>
2016-12-18 17:23:16 +1100

    Edit User Schema and add validator lib

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,7 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
@@ -8,12 +11,25 @@ var userSchema = new Schema({
   minlength: 1,
   trim: true,
   validate: {
-    validator: (email) => {
-      var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-      return emailRegex.test(email);
-    }
+    validator: validator.isEmail,
+    message: '${email} is not a valid email!'
   }
-}
+},
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  token: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 var Users = mongoose.model('Users', userSchema);
