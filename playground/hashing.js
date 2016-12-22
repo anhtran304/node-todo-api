87372f9afb7c3e72802a4861cb09ffb84eb0e624
Ken Tran <tranmr@gmail.com>
2016-12-22 13:26:08 +1100

    hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,39 +1,53 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = 'abc123';
 
-var data = {
-  id: 10
-};
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassword = '$2a$10$v6CEu6Kx4AgzmSDxL/rh8.qB2zaDknmQgmGy5V.VfKnPVVpKSproG';
 
-var decoded = jwt.verify(token, '123abc')
-console.log('Decoded by jwt.verify', decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
-
-console.log(`message: ${message}`);
-console.log(`hash: ${hash}`);
-
-var data ={
-  id: 4
-};
-
-var token = {
-  data,
-  hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-};
-
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
-
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-
-if (resultHash === token.hash) {
-  console.log('Data was not changed');
-} else {
-  console.log('Data was changed. Do not trusted');
-}
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc')
+// console.log('Decoded by jwt.verify', decoded);
+//
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+//
+// console.log(`message: ${message}`);
+// console.log(`hash: ${hash}`);
+//
+// var data ={
+//   id: 4
+// };
+//
+// var token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+//
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+//
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+//
+// if (resultHash === token.hash) {
+//   console.log('Data was not changed');
+// } else {
+//   console.log('Data was changed. Do not trusted');
+// }
